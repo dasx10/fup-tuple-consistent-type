36a879e65065c983f65725a251fcfdf86d65e007
dasx10 <dasx10@gmail.com>
2022-07-20 22:55:44 +0300

    fix: 🐛 name

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-type TupleConsistent <Tuple extends readonly unknown[]> = Tuple extends readonly [...infer StartValues, unknown]
-    ? Tuple | TupleConsistent<StartValues>
+type TupleConsistentType <Tuple extends readonly unknown[]> = Tuple extends readonly [...infer StartValues, unknown]
+    ? Tuple | TupleConsistentType<StartValues>
     : readonly [];
 
-export default TupleConsistent;
+export default TupleConsistentType;
